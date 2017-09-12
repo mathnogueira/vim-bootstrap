23ea63d3a5bce56fc99af4a54abef2aee4af5733
Matheus Barba <matheus.barba.lemaf@gmail.com>
2017-09-12 11:10:49 -0300

    Adiciona YouCompleteMe e corrige script de copiar .vimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+thisPath=`pwd`
 packages=packages.txt
 vimdir=~/.vim
 packagesPath=`pwd`/$packages
@@ -14,7 +15,14 @@ mkdir -p "$vimdir/colors"
 echo "(2/4) Installing pathogen.vim..."
 curl -LSso $vimdir/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-echo "(3/4) Installing packages..."
+echo "(3/5) Installing YouCompleteMe"
+cd $vimdir/bundle
+git clone -q https://github.com/Valloric/YouCompleteMe.git
+cd YouCompleteMe
+git submodule update --init --recursive
+./install.py
+
+echo "(4/5) Installing packages..."
 cd $vimdir/bundle
 while IFS= read -r package
 do
@@ -22,5 +30,6 @@ do
     git clone -q $package
 done < "$packagesPath"
 
-echo "(4/4) Copying .vimrc to $HOME..."
+echo "(5/5) Copying .vimrc to $HOME..."
+cd $thisPath
 cp .vimrc ~/
